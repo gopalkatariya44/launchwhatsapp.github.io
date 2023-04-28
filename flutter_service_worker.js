4a8b8038e51b0e26efdad8edb9bd5dfc82b3b6d7
Gopal Katariya <katariyagopal60@gmail.com>
2023-04-28 11:35:15 +0530

    new code v2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a9b6e856ab6a2b5ffc7049b596ccf9da",
-"index.html": "c1b2e157af7656df8e7e3fd9cea63faf",
-"/": "c1b2e157af7656df8e7e3fd9cea63faf",
-"main.dart.js": "c97dcf544cf6a17518eefd2014a7583b",
+"index.html": "aab216c3a75eb13a1898b7d98e99f225",
+"/": "aab216c3a75eb13a1898b7d98e99f225",
+"main.dart.js": "2fd70b468dc3e99716a501e8c3b44365",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
